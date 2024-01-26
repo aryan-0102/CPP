4c4a0f021188d9a2c91ccc2a6d1507da0c160a44
aryan <aryan010204@gmail.com>
2024-01-26 22:32:45 +0530

    update

---
+++ Concepts/Sorting/Bubble.cpp
@@ -0,0 +1,47 @@
+// Bubble sort is a sorting algorithm that works by repeatedly swapping adjacent elements until a sorted array is obtained
+
+#include <iostream>
+using namespace std ;
+int swap(int* a ,int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return 0;
+    }
+
+int main(){
+    int n ;
+    cout<<"Enter number of elements  : ";
+    cin>>n;
+
+    int arr[n];
+
+    cout<<"Enter values into array : "<<endl;
+
+    for(int i = 0;i<n;i++){
+        cout<<"Enter arr["<<i<<"] : ";
+        cin>>arr[i];
+    }
+    
+    cout<<"Array before sorting : "<<endl;
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    for(int i = 0;i<n;i++){
+        for(int j = 0; j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(&arr[j],&arr[j+1]);
+            }
+        }
+    }
+
+    cout<<endl<<"Array after sorting : "<<endl;
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0 ;
+}
